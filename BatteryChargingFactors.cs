389fcdbe94a942145f597393eafd24950d2892ff
RashmitaSoni <36473003+RashmitaSoni@users.noreply.github.com>
2021-03-03 14:04:58 +0530

    Update BatteryChargingFactors.cs

--- BatteryChargingFactors.cs
+++ BatteryChargingFactors.cs
@@ -11,17 +11,17 @@ namespace BatteryCharging
         static float maximumStateOfCharge_Percentage = 80;
         static float minimumChargeRate_CRate = 0.3f;
         static float maximumChargeRate_CRate = 0.8f;
-        public static bool CheckChargingTemperature(float chargingTemperature_Celsius)
+        public static void CheckChargingTemperature(float chargingTemperature_Celsius)
         {
-           return (BatteryAbnormalConditions.CheckBreachingPoint("Charging Temperature", chargingTemperature_Celsius, minimumChargingTemprature_Celsius, maximumChargingTemprature_Celsius));
+            BatteryAbnormalConditions.CheckBreachingPoint("Charging Temperature", chargingTemperature_Celsius, minimumChargingTemprature_Celsius, maximumChargingTemprature_Celsius);
         }
-        public static bool CheckChargeRate(float chargeRate_CRate)
+        public static void CheckChargeRate(float chargeRate)
         {
-            return(BatteryAbnormalConditions.CheckBreachingPoint("Charge Rate", chargeRate_CRate, minimumChargeRate_CRate, maximumChargeRate_CRate));
+            BatteryAbnormalConditions.CheckBreachingPoint("Charge Rate", chargeRate, minimumChargeRate_CRate, maximumChargeRate_CRate);
         }
-        public static bool CheckStateOfCharge(float stateOfCharge_percentage)
+        public static void CheckStateOfCharge(float stateOfCharge_percentage)
         {
-            return(BatteryAbnormalConditions.CheckBreachingPoint("State of Charge", stateOfCharge_percentage, minimumStateOfCharge_Percentage, maximumStateOfCharge_Percentage));
+            BatteryAbnormalConditions.CheckBreachingPoint("State of Charge", stateOfCharge_percentage, minimumStateOfCharge_Percentage, maximumStateOfCharge_Percentage);
         }
     }
 }
