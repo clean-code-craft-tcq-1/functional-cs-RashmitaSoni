ace227785ad3c6a01277342fe894d6403c8a4092
RashmitaSoni <36473003+RashmitaSoni@users.noreply.github.com>
2021-03-03 14:02:41 +0530

    Update BatteryChargingFactors.cs

--- BatteryChargingFactors.cs
+++ BatteryChargingFactors.cs
@@ -9,19 +9,19 @@ namespace BatteryCharging
         static float maximumChargingTemprature_Celsius = 45;
         static float minimumStateOfCharge_Percentage = 20;
         static float maximumStateOfCharge_Percentage = 80;
-        static float minimumChargeRate = 0.3f;
-        static float maximumChargeRate = 0.8f;
-        public static void CheckChargingTemperature(float chargingTemperature_Celsius)
+        static float minimumChargeRate_CRate = 0.3f;
+        static float maximumChargeRate_CRate = 0.8f;
+        public static bool CheckChargingTemperature(float chargingTemperature_Celsius)
         {
-            BatteryAbnormalConditions.CheckBreachingPoint("Charging Temperature", chargingTemperature_Celsius, minimumChargingTemprature_Celsius, maximumChargingTemprature_Celsius);
+           return (BatteryAbnormalConditions.CheckBreachingPoint("Charging Temperature", chargingTemperature_Celsius, minimumChargingTemprature_Celsius, maximumChargingTemprature_Celsius));
         }
-        public static void CheckChargeRate(float chargeRate)
+        public static bool CheckChargeRate(float chargeRate_CRate)
         {
-            BatteryAbnormalConditions.CheckBreachingPoint("Charge Rate", chargeRate, minimumChargeRate, maximumChargeRate);
+            return(BatteryAbnormalConditions.CheckBreachingPoint("Charge Rate", chargeRate_CRate, minimumChargeRate_CRate, maximumChargeRate_CRate));
         }
-        public static void CheckStateOfCharge(float stateOfCharge_percentage)
+        public static bool CheckStateOfCharge(float stateOfCharge_percentage)
         {
-            BatteryAbnormalConditions.CheckBreachingPoint("State of Charge", stateOfCharge_percentage, minimumStateOfCharge_Percentage, maximumStateOfCharge_Percentage);
+            return(BatteryAbnormalConditions.CheckBreachingPoint("State of Charge", stateOfCharge_percentage, minimumStateOfCharge_Percentage, maximumStateOfCharge_Percentage));
         }
     }
 }
