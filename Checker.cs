f1953087ac02ffd46d90b193cbafcb71027c1319
RashmitaSoni <36473003+RashmitaSoni@users.noreply.github.com>
2021-03-03 10:55:40 +0530

    Update Checker.cs

--- Checker.cs
+++ Checker.cs
@@ -1,42 +1,14 @@
 using System;
 using System.Diagnostics;
-using BatteryCharging;
 
 namespace BatteryCharging
 {
     public class Checker
     {
-       static bool IsBatteryCharging(float chargingTemperature_Celsius, float stateOfCharge_percentage, float chargeRate)
-        {
-            bool IstemperatureCorrect = BatteryChargingFactors.CheckChargingTemperature(chargingTemperature_Celsius);
-            bool IsstateofChargeCorrect = BatteryChargingFactors.CheckStateOfCharge(stateOfCharge_percentage);
-            bool IschargeRateCorrect = BatteryChargingFactors.CheckChargeRate(chargeRate);
-            return (IstemperatureCorrect && IsstateofChargeCorrect && IschargeRateCorrect);
-        }
-       static void ExpectResult(bool expression, string expectresult)
-        {
-            switch (expectresult)
-            {
-                case "ExpectTrue":
-                    if (!expression)
-                    {
-                        Console.WriteLine("Expected true, but got false");
-                        Environment.Exit(1);
-                    }
-                    break;
-                case "ExpectFalse":
-                    if (expression)
-                    {
-                        Console.WriteLine("Expected false, but got true");
-                        Environment.Exit(1);
-                    }
-                    break;
-            }
-        }
         public static int Main()
         {
-            ExpectResult(IsBatteryCharging(25, 70, 0.7f), "ExpectTrue");
-            ExpectResult(IsBatteryCharging(50, 85, 0.0f), "ExpectFalse");
+            BatteryChargingTest.BatteryChargingTestFactors(25, 70, 0.7f);
+            BatteryChargingTest.BatteryChargingTestFactors(50, 85, 0.0f);
             Console.WriteLine("All ok");
             return 0;
         }
