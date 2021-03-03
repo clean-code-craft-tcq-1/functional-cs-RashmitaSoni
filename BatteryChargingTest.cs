3d8ef7bae9e6f53cc1ad977e8a990eeb13ce19f3
RashmitaSoni <36473003+RashmitaSoni@users.noreply.github.com>
2021-03-03 14:05:41 +0530

    Update BatteryChargingTest.cs

--- BatteryChargingTest.cs
+++ BatteryChargingTest.cs
@@ -5,11 +5,11 @@ namespace BatteryCharging
 {
     public class BatteryChargingTest
     {
-       public static void BatteryChargingTestFactors(float chargingTemperature_Celsius, float stateOfCharge_percentage, float chargeRate)
+       public static void BatteryChargingTestFactors(float chargingTemperature_Celsius, float stateOfCharge_percentage, float chargeRate_CRate)
         {
             BatteryChargingFactors.CheckChargingTemperature(chargingTemperature_Celsius);
             BatteryChargingFactors.CheckStateOfCharge(stateOfCharge_percentage);
-            BatteryChargingFactors.CheckChargeRate(chargeRate);
+            BatteryChargingFactors.CheckChargeRate(chargeRate_CRate);
         }
     }
 }
