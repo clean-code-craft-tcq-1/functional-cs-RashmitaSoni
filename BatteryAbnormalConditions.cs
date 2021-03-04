fb651b577952cf112c38b4c8d24a647f7f8ae277
RashmitaSoni <36473003+RashmitaSoni@users.noreply.github.com>
2021-03-04 10:25:47 +0530

    Update BatteryAbnormalConditions.cs

--- BatteryAbnormalConditions.cs
+++ BatteryAbnormalConditions.cs
@@ -7,20 +7,27 @@ namespace BatteryCharging
     {
         static readonly string LowBreach = "Breaching Point is Low";
         static readonly string HighBreach = "Breaching Point is High";
+        static bool FaultyBattery = false;
         public static void CheckBreachingPoint(string factorName, float factorValue, float minimumFactorLimit, float maximumFactorLimit)
-        {
+        { 
             if (factorValue < minimumFactorLimit)
             {
                 DisplayAbnormalConditions(factorName, minimumFactorLimit, maximumFactorLimit,LowBreach);
+                FaultyBattery = true;
             }
             if (factorValue > maximumFactorLimit)
             {
                 DisplayAbnormalConditions(factorName, minimumFactorLimit, maximumFactorLimit,HighBreach);
+                FaultyBattery = true;
             }
         }
         public static void DisplayAbnormalConditions(string factorName, float minimumFactorLimit, float maximumFactorLimit, string breachPoint)
         {
             Console.WriteLine("{0} is out of range!\nRange is ({1},{2})\n{3}",factorName,minimumFactorLimit,maximumFactorLimit,breachPoint);
+        }
+        public static bool GetBatteryStatus()
+        {
+            return FaultyBattery;
         }
     }
 }
