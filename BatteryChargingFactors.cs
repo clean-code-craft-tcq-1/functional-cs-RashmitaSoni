ca74c52ed9c8eb6e2991a76b5ca9037ec135b8c2
RashmitaSoni <36473003+RashmitaSoni@users.noreply.github.com>
2021-03-14 21:29:10 +0530

    Update BatteryChargingFactors.cs

--- BatteryChargingFactors.cs
+++ BatteryChargingFactors.cs
@@ -5,23 +5,14 @@ namespace BatteryCharging
 {
     public class BatteryChargingFactors
     {
-        static float minimumChargingTemprature_Celsius = 0;
-        static float maximumChargingTemprature_Celsius = 45;
-        static float minimumStateOfCharge_Percentage = 20;
-        static float maximumStateOfCharge_Percentage = 80;
-        static float minimumChargeRate_CRate = 0.3f;
-        static float maximumChargeRate_CRate = 0.8f;
-        public static void CheckChargingTemperature(float chargingTemperature_Celsius)
-        {
-            BatteryAbnormalConditions.CheckBreachingPoint("Charging Temperature", chargingTemperature_Celsius, minimumChargingTemprature_Celsius, maximumChargingTemprature_Celsius);
-        }
-        public static void CheckChargeRate(float chargeRate_CRate)
-        {
-            BatteryAbnormalConditions.CheckBreachingPoint("Charge Rate", chargeRate_CRate, minimumChargeRate_CRate, maximumChargeRate_CRate);
-        }
-        public static void CheckStateOfCharge(float stateOfCharge_percentage)
-        {
-            BatteryAbnormalConditions.CheckBreachingPoint("State of Charge", stateOfCharge_percentage, minimumStateOfCharge_Percentage, maximumStateOfCharge_Percentage);
-        }
+        public static float minimumChargingTemprature_Celsius = 0;
+        public static float maximumChargingTemprature_Celsius = 45;
+        public static float minimumStateOfCharge_Percentage = 20;
+        public static float maximumStateOfCharge_Percentage = 80;
+        public static float minimumChargeRate_CRate = 0.3f;
+        public static float maximumChargeRate_CRate = 0.8f;
+        public static readonly string ChargingTemperature = "Charging Temperature";
+        public static readonly string StateOfCharge = "State of Charge";
+        public static readonly string ChargeRate = "Charge Rate";
     }
 }
