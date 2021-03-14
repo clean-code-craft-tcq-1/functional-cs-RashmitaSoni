6f7e8dda8fdb42e989f5f9edfd3b10b4b5164b5d
RashmitaSoni <36473003+RashmitaSoni@users.noreply.github.com>
2021-03-14 21:28:51 +0530

    Update BatteryChargingTest.cs

--- BatteryChargingTest.cs
+++ BatteryChargingTest.cs
@@ -5,16 +5,18 @@ namespace BatteryCharging
 {
     public class BatteryChargingTest
     {
+       static bool IsBatteryFaulty;
        public static void BatteryChargingTestFactors(float chargingTemperature_Celsius, float stateOfCharge_percentage, float chargeRate_CRate)
         {
-            BatteryChargingFactors.CheckChargingTemperature(chargingTemperature_Celsius);
-            BatteryChargingFactors.CheckStateOfCharge(stateOfCharge_percentage);
-            BatteryChargingFactors.CheckChargeRate(chargeRate_CRate);
+            IsBatteryFaulty =
+                BatteryAbnormalConditions.CheckChargingFactors(BatteryChargingFactors.ChargingTemperature, chargingTemperature_Celsius, BatteryChargingFactors.minimumChargingTemprature_Celsius, BatteryChargingFactors.maximumChargingTemprature_Celsius)
+                | BatteryAbnormalConditions.CheckChargingFactors(BatteryChargingFactors.StateOfCharge, stateOfCharge_percentage, BatteryChargingFactors.minimumStateOfCharge_Percentage, BatteryChargingFactors.maximumStateOfCharge_Percentage)
+                | BatteryAbnormalConditions.CheckChargingFactors(BatteryChargingFactors.ChargeRate, chargeRate_CRate, BatteryChargingFactors.minimumChargeRate_CRate, BatteryChargingFactors.maximumChargeRate_CRate);
             DisplayBatteryStatus();
         }
        public static void DisplayBatteryStatus()
         {
-            string BatteryStatus = (BatteryAbnormalConditions.GetBatteryFaultyStatus() == true) ? "Battery is faulty" : "Battery is OK";
+            string BatteryStatus = (IsBatteryFaulty == true) ? "Battery is faulty" : "Battery is OK";
             Console.WriteLine(BatteryStatus);
         }
     }
